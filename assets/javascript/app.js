531f4ec0fd7ac5705576c08d4f6d1d6d63cd7731
Robin Bertuccelli <robin.bertuccelli@gmail.com>
2018-04-14 07:29:05 -0500

    Updates and improvements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,3 +3,9 @@ $(".landing-page").click(function() {
     window.location = $(this).find("a").attr("href");
     return false;
 });
+//stickyfill
+var stickyElements = document.getElementsByClassName('sticky');
+
+for (var i = stickyElements.length - 1; i >= 0; i--) {
+    Stickyfill.add(stickyElements[i]);
+}
